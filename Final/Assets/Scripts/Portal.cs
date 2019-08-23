c874feab617468099c4351e66c2cefc207891fc4
peter <peterzhou43@gmail.com>
2019-08-23 08:51:36 -0700

    Part 6

---
+++ Final/Assets/Scripts/Portal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Transform Destination;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(Destination!= null)
+        {
+            GameObject other = collision.gameObject;
+            Player player = other.GetComponent<Player>();
+
+            if (player != null)
+            {
+                player.transform.position = Destination.position;
+            }
+        }
+    }
+}
